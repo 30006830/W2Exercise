4baba568bab6a51a8b61dece709ce8ff299ca83a
Blake Jensen <30006830@student.toiohomai.ac.nz>
2019-03-06 08:58:26 +1300

    Week 2 1st exercise

---
+++ Week2Exercise/Week2Exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string gender;
+            string address;
+
+            Console.WriteLine("Please enter your name here: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Please enter your age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please state your Gender: ");
+            gender = Console.ReadLine();
+
+            Console.WriteLine("Finally enter your Address: ");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Outputting Results....");
+            Console.ReadLine();
+
+            Console.WriteLine("Outputs: \n Name: {0} \n Age: {1} \n Gender: {2} \n Address (Email or Street Address): {3} ",name, age, gender, address );
+
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
