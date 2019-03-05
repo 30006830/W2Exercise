e5ce071ff8629b8fd879ba105dc473afb7446a73
30006830 <48032096+30006830@users.noreply.github.com>
2019-03-06 09:12:28 +1300

    Update Program.cs

--- Week2Exercise/Week2Exercise/Program.cs
+++ Week2Exercise/Week2Exercise/Program.cs
@@ -10,16 +10,20 @@ namespace Week2Exercise
     {
         static void Main(string[] args)
         {
+            //list of variables needed for the program, this will store the data for name, age, gender, address.
             string name;
             int age;
             string gender;
             string address;
-
+            
+            /*Requests the user input their name*/
             Console.WriteLine("Please enter your name here: ");
+            //takes the users name and stores it in the variable, name.
             name = Console.ReadLine();
 
-            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello " + name); //prints "Hello" and then displays the users name.
 
+            /*All these take the users input from the answers to each request and stores them all in specific variables*/
             Console.WriteLine("Please enter your age: ");
             age = int.Parse(Console.ReadLine());
 
@@ -32,6 +36,7 @@ namespace Week2Exercise
             Console.WriteLine("Outputting Results....");
             Console.ReadLine();
 
+            //Outputs the variables stored in a listed format.
             Console.WriteLine("Outputs: \n Name: {0} \n Age: {1} \n Gender: {2} \n Address (Email or Street Address): {3} ",name, age, gender, address );
 
             
